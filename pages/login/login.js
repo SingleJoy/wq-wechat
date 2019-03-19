aae67480a661190186e1386b6125a8fcd403640d
wangning <wangning@51signing.com>
2019-03-20 01:52:16 +0800

    login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,19 +1,22 @@
 // pages/login/login.js
 import { loginWesign} from '../../wxapi/api.js'
-
+import util from '../../utils/util.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    password:'',
+    password:'',
+    usernameErr:'',
+    passwordErr:''
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(util)
   },
 
   /**
@@ -57,7 +60,6 @@ Page({
   onReachBottom: function () {
 
   },
-
   /**
    * 用户点击右上角分享
    */
@@ -66,5 +68,47 @@ Page({
   },
   formSubmit:function(e){
     console.log(e)
-  }
+  },
+  //输入框事件
+  bindUsernameInput: function (e) {
+    var user_val =  e.detail.value;
+    this.setData({
+        username: user_val,
+    });
+    
+  },
+  bindPasswordInput: function (e) {
+    var pwd_val =  e.detail.value;
+    this.setData({
+      password: pwd_val
+    });
+  },
+  //清除事件
+  clearInput: function (e) {
+    switch (e.currentTarget.id) {
+      case 'clear-username':
+        this.setData({
+          username: ''
+        });
+        break;
+      case 'clear-password':
+        this.setData({
+          password: ''
+        });
+        break;
+      case 'clear-code':
+        this.setData({
+          code: ''
+        });
+        break;
+    }
+  },
+  //长按识别二维码
+    previewImage:function(e) {
+        var current = e.target.dataset.src;
+        wx.previewImage({
+            current: current,
+            urls: [current]
+        })
+    }
 })
