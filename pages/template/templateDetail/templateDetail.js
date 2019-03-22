4dc6bbcb5166d7111dc72cbae45507d89d7fde7f
Abriams <Abriams@163.com>
2019-03-22 16:11:04 +0800

    修改

--- pages/template/templateDetail/templateDetail.js
+++ pages/template/templateDetail/templateDetail.js
@@ -10,11 +10,7 @@ Page({
 
   ImmediatelyStart: function() {
     wx.navigateTo({
-<<<<<<< HEAD
       url: '../templateSet/templateSet',
-=======
-      url: '../templateAddInfo/templateAddInfo',
->>>>>>> f7d31cafd50b3d985ee54b6a30232fd1f969089c
     })
   },
   /**
