08fdeb92fe1db7c331215b04792f87d59d3c0658
SingleJoy <2511881919@qq.com>
2019-03-22 14:24:55 +0800

    合同列表

--- pages/contract/contract.js
+++ pages/contract/contract.js
@@ -5,13 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      folder:'默认文件夹',
+      contractType:'对个人合同',
+      accountType:'全部账号',
+      currentTab:0,
+      sum:1,
+      model:true,
+      selected:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+
+  tabNav(e){
+    console.log(e.currentTarget.dataset.current)
+      let current=e.currentTarget.dataset.current;
+      if(current==this.data.selected){
+          this.setData({
+              selected:0
+          })
+      }else{
+          this.setData({
+              selected:current
+          })
+      }
+
+  },
   onLoad: function (options) {
+      console.log("onLoad")
 
   },
 
@@ -19,48 +41,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+      console.log("onReady")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+      console.log("onShow")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+      console.log("onHide")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+      console.log("onUnload")
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+      console.log("onPullDownRefresh")
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+      console.log("onReachBottom")
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+      console.log("onShareAppMessage")
   }
 })
