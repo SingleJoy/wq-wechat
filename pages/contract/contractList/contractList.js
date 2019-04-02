1d48a91255092b87a97454a79c5582246fd8b6b6
SingleJoy <2511881919@qq.com>
2019-04-02 16:54:06 +0800

    list

--- pages/contract/contractList/contractList.js
+++ pages/contract/contractList/contractList.js
@@ -425,6 +425,7 @@ Page({
 
         let searchParam=app.globalData.searchParam;
         let search=Object.keys(searchParam);
+        //判断页面是从哪个进入，如果是从contractSearch 保存从contractSearch 获取app.data存储的数据
         if (search.length>0) {
             // console.log(app.globalData.searchParam);
             Object.keys(searchParam).forEach((key)=>{
