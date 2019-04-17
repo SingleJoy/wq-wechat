8788e499879e1cc144f704323c4a34a2112a53cb
SingleJoy <2511881919@qq.com>
2019-04-17 10:52:43 +0800

    配置文件

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   API_BASE_URL: 'https://testwesign.zqsign.com',      //测试环境
   BASE_URL:'https://testwesign.zqsign.com',           //测试环境
   PROJECT_NAME : '/api',                              //测试环境
-    
+
     // API_BASE_URL: 'https://www.zqsign.com',      //正式环境
     // BASE_URL:'https://www.zqsign.com',           //正式环境
     // PROJECT_NAME : '/api'                         //正式环境
--- wxapi/main.js
+++ wxapi/main.js
@@ -6,47 +6,48 @@ const util = require('../utils/util.js')
 
 const request = (url, method, data, contentType) => {
 
-  //接口具体地址
-  let _url = API_URL + url;
-  // 默认是'application/json',如图片等内容需要'image/png' 需单独在接口中传
-  //post 需要application/x-www-form-urlencoded 格式
-  let content_type = contentType?contentType:(method.toLowerCase() == 'get'?'application/json':'application/x-www-form-urlencoded')
-  return new Promise((resolve, reject) => {
+    //接口具体地址
+    let _url = API_URL + url;
+    // 默认是'application/json',如图片等内容需要'image/png' 需单独在接口中传
+    //post 需要application/x-www-form-urlencoded 格式
+    let content_type = contentType?contentType:(method.toLowerCase() == 'get'?'application/json':'application/x-www-form-urlencoded')
+    return new Promise((resolve, reject) => {
 
-    wx.request({
-        url: _url,
-        method: method,
-        data: data,
+        wx.request({
+            url: _url,
+            method: method,
+            data: data,
             header: {
-              'Content-Type':content_type,
-              'cookie': 'wesign_token=' + wx.getStorageSync("wesign_token") + ';SESSION=62903821-116f-483a-9fcd-8ceac0eca37a; Path=/; HttpOnly'
+                'Content-Type':content_type,
+                'cookie':wx.getStorageSync("wesign_token")
             },
-        success(res) {
-            // let wesign_token=res.header["Set-Cookie"];
-            // if(wesign_token){
-            //     wx.setStorageSync("wesign_token",wesign_token)
-            // }
+            success(res) {
 
-            if (res.statusCode == 200) {
+                let wesign_token=res.header["Set-Cookie"];
+                if(wesign_token){
+                    wx.setStorageSync("wesign_token",wesign_token)
+                }
 
-            if (res.data.sessionStatus == '000000') {
+                if (res.statusCode == 200) {
 
-                // return util.getToken()
-            } else {
-                resolve(res)
+                    if (res.data.sessionStatus == '000000') {
+
+                        // return util.getToken()
+                    } else {
+                        resolve(res)
+                    }
+                } else {
+                    reject(res.errMsg)
+                }
+            },
+            fail(error) {
+                reject(error)
+            },
+            complete(aaa) {
+                // 加载完成
             }
-            } else {
-                reject(res.errMsg)
-            }
-        },
-        fail(error) {
-            reject(error)
-        },
-        complete(aaa) {
-            // 加载完成
-        }
+        })
     })
-  })
 }
 
 module.exports = request;
